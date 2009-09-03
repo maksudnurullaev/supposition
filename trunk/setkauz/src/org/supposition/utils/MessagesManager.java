d8cb7d90b6d45c5edc82b6a0923dd59f17e2e2bb
maksud.nurullaev <maksud.nurullaev@637863ca-8676-11de-bed6-6d316fbe0868>
2009-09-03 09:33:28 +0000

    Locading and cashing languages functionality and save locales for each session

--- trunk/setkauz/src/org/supposition/utils/MessagesManager.java
+++ trunk/setkauz/src/org/supposition/utils/MessagesManager.java
@@ -8,26 +8,31 @@ import org.supposition.text.TextManager;
 
 public final class MessagesManager {
 	public static Log _log = LogFactory.getLog("org.supposition.utils.WebMessageFactory");	
-	private static WebApplicationContext wac = ContextLoader.getCurrentWebApplicationContext();
+	private static WebApplicationContext _wac = ContextLoader.getCurrentWebApplicationContext();
 	
-	private static TextManager _textManager = (TextManager) wac.getBean(Constants._beans_text_manager);
+	private static TextManager _textManager = (TextManager) _wac.getBean(Constants._beans_text_manager);
 	
 	
 	// *** TEXT MANAGER part ****
-	public static  String getText(String inKey){
-		return _textManager.getTextByKey(inKey);
+	public static String getText(String inKey){
+		return _textManager.getTextByKey(inKey, SessionManager.getSessionLocale());
 	}
 	
 	// *** MESSAGE SENDER part ****
 	public static boolean hasMessageByKey(String inKey){
-		return _textManager.hasKey(inKey);
+		return _textManager.hasKey(inKey, SessionManager.getSessionLocale());
 	}
 	
 	public static void changeLocale(String inLocale) {
+		// Save session
+		SessionManager.setSessionLocale(inLocale);
+		
+		// Set locale
 		_textManager.setLocale(inLocale);
 	}
 
 	public static String getLocale() {
-		return _textManager.getLocale();
-	}	
+		return SessionManager.getSessionLocale();
+	}
+	
 }
