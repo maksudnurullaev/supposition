5ab139d0068753719f6309d48630bf1f183c7ac3
maksud.nurullaev <maksud.nurullaev@637863ca-8676-11de-bed6-6d316fbe0868>
2010-03-12 11:21:04 +0000

    Delete unneccessary files!

--- setkauz/WebContext/scripts/Gup.js
+++
@@ -1,1 +0,0 @@
-Namespace("Gup");Gup.testURL=function(name,url){if(!url){url=window.location.href}name=name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");var results=new RegExp("[\\?&]"+name+"=([^&#]*)").exec(url);if(results==null){return null}else{return decodeURIComponent(results[1].replace(/\+/g," "))}};
